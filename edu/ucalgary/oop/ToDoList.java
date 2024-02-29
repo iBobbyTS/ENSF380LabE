f996b9e79153c621b1cde0728ae7ddc7e572b504
iBobbyTS <ibobbyts@gmail.com>
2024-02-29 15:15:17 -0700

    ToDoList: completeTask, deleteTask

--- edu/ucalgary/oop/ToDoList.java
+++ edu/ucalgary/oop/ToDoList.java
@@ -1,15 +1,18 @@
 package edu.ucalgary.oop;
 import java.util.List;
+import java.util.Dictionary;
 
 
 class ToDoList implements IToDoList {
     private List<Task> taskList;
+    private Dictionary<String, Task> taskDict;
     public ToDoList() {
 
     }
 
     public void addTask(Task task){
         taskList.add(task);
+        taskDict.put(task.getId(), task);
     }
 
     public List<Task> listTasks(){
@@ -17,11 +20,11 @@ class ToDoList implements IToDoList {
     }
 
     public void completeTask(String id){
-        
+        taskDict.get(id).setIscompleted(true);
     }
 
     public void deleteTask(String taskId) {
-
+        taskList.remove(taskDict.get(taskId));
     }
 
     public void editTask(String taskId, String title, boolean completed) {
